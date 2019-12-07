8314e4cfa86162d2b73a6ccbeaf7b1bfdba9d56f
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-12-07 10:04:50 -0800

    awesome use of the __str__

--- python_playground/python_class_representation.py
+++ python_playground/python_class_representation.py
@@ -6,6 +6,19 @@ class Car:
     def __str__(self):
         return 'a {self.color} car'.format(self=self)
 
+my_car = Car('red', 5432)
+print(my_car)
+
+
+
+
+
+
+
+
+# dunder str or __str__ converts objects into strings -- pythonic way of doing this  
+# str(my_car) # will call the str method
+# {}.format(my_car)
 
 # print(my_car)
 # print(my_car.color, my_car.milage)  #incorrect
