01810ec516cc43ab50608cadc999bdf2b3b98208
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-12-09 15:04:53 -0800

    super() class fun with shapes

--- python_playground/oop_path/super_shapes_2.py
+++ python_playground/oop_path/super_shapes_2.py
@@ -1,8 +1,13 @@
-class Square(Rectangel)
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        seld.width = width
+
+class Square(Rectangle):
     def __init__(self, length):
         super().__init__(length, length)
 
-class Cube(Square)
+class Cube(Square):
     def surface_area(self):
         face_area = super().area()
         return face_area * 6
